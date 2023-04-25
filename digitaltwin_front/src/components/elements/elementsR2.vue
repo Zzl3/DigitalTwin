14c355fcc7338e6255b96cbb48b74da7dfa48a0b
lei <xiang.lei.se@foxmail.com>
2023-04-25 17:45:11 +0800

    完成第二张图 右边两个图表 调整整个图形样式

---
+++ digitaltwin_front/src/components/elements/elementsR2.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="table-container">
+        <el-table :data="tableData" :row-style="{ height: '50px' }"
+            :header-cell-style="{ 'text-align': 'center', 'border': 'none' }" :cell-style="rowStyle">
+            <el-table-column prop="airStation" label="空压站" min-width="30%">
+                <!-- 用插槽的方法来改变颜色! -->
+                <template slot-scope="scope">
+                    <span :style="{ color: '#1953FC' }">{{ scope.row.airStation }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="time1" label="第一次补气" min-width="30%">
+                <template slot-scope="scope">
+                    <span :style="{ color: '#1953FC' }">{{ scope.row.time1 }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="time2" label="第二次补气" min-width="30%">
+                <template slot-scope="scope">
+                    <span :style="{ color: '#1953FC' }">{{ scope.row.time2 }}</span>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        // eslint-disable-next-line
+        rowStyle({ row, column, rowIndex, columnIndex }) {
+            //设置表格每行间隔边框
+            if (rowIndex % 2 === 0) {
+                if (columnIndex === 0) {
+                    return "text-align:center;border:2px solid #1953FC;border-right-width:0;"
+                }
+                else if (columnIndex === 1) {
+                    return "text-align:center;border:2px solid #1953FC;border-right-width:0;border-left-width:0;"
+                }
+                else {
+                    return "text-align:center;border:2px solid #1953FC;border-left-width:0;"
+                }
+            }
+            if (rowIndex % 2 === 1) {
+                return "text-align:center;border:none;"
+            }
+        },
+    },
+    data() {
+        return {
+            tableData: [{
+                airStation: '空压站1',
+                time1: '---------- KPa',
+                time2: '---------- KPa',
+            }, {
+                daairStationte: '',
+                time1: '',
+                time2: ''
+            }, {
+                airStation: '空压站2',
+                time1: '---------- KPa',
+                time2: '---------- KPa',
+            }, {
+                airStation: '',
+                time1: '',
+                time2: ''
+            },
+            {
+                airStation: '空压站3',
+                time1: '---------- KPa',
+                time2: '---------- KPa',
+            }, {
+                airStation: '',
+                time1: '',
+                time2: ''
+            }]
+        }
+    }
+}
+</script>
+
+<style scoped>
+/*最外层透明*/
+/deep/.el-table {
+    background-color: transparent !important;
+}
+
+/deep/.el-table__expanded-cell {
+    background-color: transparent !important;
+}
+
+/* 去除最外层的下边框 */
+/deep/.el-table::before {
+    height: 0px;
+}
+
+
+/* 表格内背景颜色 */
+/* 表头单元格 */
+/deep/.el-table th {
+    background-color: rgba(75, 121, 255, 0.1) !important;
+    color: #000 !important;
+}
+
+/* 一行 */
+/deep/.el-table tr {
+    background-color: transparent !important;
+    width: 100%;
+}
+
+/* 一个单元格 */
+/deep/.el-table td {
+    background-color: transparent !important;
+    font-weight: 10000;
+}
+</style>
--- digitaltwin_front/src/views/waysSelect.vue
+++ digitaltwin_front/src/views/waysSelect.vue
@@ -1,83 +1,95 @@
 <template>
-<div class="about">
-    <div class="content" id="box2l1">
-        <div style="text-align: left">
-            <i class="el-icon-news titletext"></i>
-            <p class="titletext">优化后开关站时间</p>
+    <div class="about">
+        <div class="content" id="box2l1">
+            <div style="text-align: left">
+                <i class="el-icon-news titletext"></i>
+                <p class="titletext">优化后开关站时间</p>
+            </div>
+            <EchartsL1></EchartsL1>
         </div>
-        <EchartsL1 style="margin-top: -15px; position: absolute"></EchartsL1>
-    </div>
-    <div class="content" id="box2l2">
-        <div style="text-align: left">
-            <i class="el-icon-news titletext"></i>
-            <p class="titletext">优化前后耗电量</p>
+        <div class="content" id="box2l2">
+            <div style="text-align: left">
+                <i class="el-icon-news titletext"></i>
+                <p class="titletext">优化前后耗电量</p>
+            </div>
+            <EchartsL2></EchartsL2>
         </div>
-        <EchartsL2 style="margin-top: -50px; position: absolute"></EchartsL2>
-    </div>
-    <div class="content" id="box2r1">
-        <div style="text-align: left">
-            <i class="el-icon-news titletext"></i>
-            <p class="titletext">设置空压站开关时间</p>
+        <div class="content" id="box2r1">
+            <div style="text-align: left">
+                <i class="el-icon-news titletext"></i>
+                <p class="titletext">设置空压站开关时间</p>
+            </div>
+            <ElementR1></ElementR1>
+
+        </div>
+        <div class="content" id="box2r2">
+            <div style="text-align: left">
+                <i class="el-icon-news titletext"></i>
+                <p class="titletext">设置补气压力</p>
+            </div>
+            <ElementR2></ElementR2>
         </div>
     </div>
-    <div class="content" id="box2r2">
-        <div style="text-align: left">
-            <i class="el-icon-news titletext"></i>
-            <p class="titletext">设置补气压力</p>
-        </div>
-    </div>
-</div>
 </template>
 
 <script>
 import EchartsL1 from "@/components/echarts/echarts2L1.vue";
 import EchartsL2 from "@/components/echarts/echarts2L2.vue";
-
+import ElementR1 from "@/components/elements/elementsR1";
+import ElementR2 from "@/components/elements/elementsR2";
 export default {
     components: {
-    EchartsL1,
-    EchartsL2,
-  },
+        EchartsL1,
+        EchartsL2,
+        ElementR1,
+        ElementR2
+    },
 }
 </script>
 
-<style scoped>
+<style>
 #box2l1 {
-    width: 740px;
-    height: 330px;
+    width: 959px;
+    height: 438px;
     position: absolute;
-    left: 0;
-    top: 0;
+    left: 44px;
+    top: 30px;
+    margin: 0 0;
 }
-  
+
 #box2l2 {
-    width: 740px;
-    height: 330px;
+    width: 959px;
+    height: 438px;
     position: absolute;
-    left: 0;
-    top: 370px;
+    left: 44px;
+    top: 498px;
+    margin: 0 0;
 }
 
 #box2r1 {
-    width: 740px;
-    height: 330px;
+    width: 837px;
+    height: 438px;
     position: absolute;
-    right: 0;
-    top: 0px;
+    top: 30px;
+    margin: 0 0;
+    left: 1038px;
 }
 
 #box2r2 {
-    width: 740px;
-    height: 330px;
+    width: 837px;
+    height: 438px;
     position: absolute;
-    right: 0;
-    top: 370px;
+    margin: 0 0;
+    left: 1038px;
+    top: 498px;
 }
+
 .content {
     border-radius: 10px;
     background: rgba(255, 255, 255, 0.5);
     box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
 }
+
 .titletext {
     display: inline-block;
     margin-left: 10px;
